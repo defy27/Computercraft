aeb9b1858d4b5223a185abcd9facd5dc7dc5f728
defy27 <114071866+defy27@users.noreply.github.com>
2024-04-08 17:14:28 +0200

    Create turtle.lua

---
+++ turtle.lua
@@ -0,0 +1,14 @@
+function MineDown()
+  while turtle.inspectDown() == "minecraft:bedrock" do
+    turtle.digDown
+  end
+  while turtle.inspectDown() == "minecraft:bedrock" do
+    turtle.up
+  end
+end
+
+if turtle.inspectDown()
+
+function MineVein()
+end
+  
